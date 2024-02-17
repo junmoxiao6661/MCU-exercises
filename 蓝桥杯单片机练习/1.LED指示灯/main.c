7ecf46cb435cd3d86fd172a17f9a7b595ff9c311
junmoxiao6661 <THEBO@8ymj6p.onmicrosoft.com>
2024-02-17 23:51:01 +0800

    commit

--- 蓝桥杯单片机练习/1.LED指示灯/main.c
+++ 蓝桥杯单片机练习/1.LED指示灯/main.c
@@ -6,30 +6,47 @@ sbit HC138_A=P2^5;
 sbit HC138_B=P2^6;
 sbit HC138_C=P2^7;
 
-void Delay(u16 t)
+//74HC138译码器工作原理为按CBA顺序输入0或1表示7个位置，111是Y7，本次用的Y4是110
+void Delay(u16 xms)		//@11.0592MHz
 {
-	while(t--);
-	while(t--);
+
+	while(xms--)
+	{
+		unsigned char i, j;
+
+		_nop_();
+		i = 2;
+		j = 199;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
 }
+
 void LEDRunning()
 {
 	u8 i;
 	HC138_C=1;
 	HC138_B=0;
 	HC138_A=0;
-	/*for(i=1;i<=8;i++)
+	for(i=1;i<=3;i++)
 	{
-		P0=0xFF<<i;
-		Delay(60000);
-		Delay(60000);
+		P0=0x00;
+		Delay(1000);
+		P0=0xFF;
+		Delay(1000);
 	}
 	for(i=1;i<=8;i++)
 	{
-		P0=(0x01>>i)+1;
-		Delay(60000);
-		Delay(60000);
-	}*/
-	P0=0x80;
+		P0=(0xFF<<i);//最低位是第一个灯
+		Delay(1000);
+	}
+	for(i=1;i<=8;i++)
+	{
+		P0=~(0xFF<<i);//最低位是第一个灯
+		Delay(1000);
+	}
 }
 void main()
 {
